7e2683e7af8238a571fcd66e60910d5585b40f4c
githussein <mo.samy.hussein@gmail.com>
2021-09-14 12:55:42 +0200

    Pull down to refresh Manage Coupond list

--- lib/screens/manage_coupons_screen.dart
+++ lib/screens/manage_coupons_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:wafar_cash/screens/edit_coupon_screen.dart';
-import 'package:wafar_cash/widgets/coupon_item.dart';
 
+import '../screens/edit_coupon_screen.dart';
 import '../providers/coupons_provider.dart';
 import '../widgets/admin_coupon_item.dart';
 import '../widgets/app_drawer.dart';
 
 class ManageCouponsScreen extends StatelessWidget {
   static const routeName = '/manage-coupons';
+
+  //function to handle Refresh Indicator
+  Future<void> _refreshProducts(BuildContext ctx) async {
+    await Provider.of<CouponsProvider>(ctx, listen: false).fetchCoupons();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,21 +30,24 @@ class ManageCouponsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(
-          (10),
-        ),
-        child: ListView.builder(
-          itemCount: couponsData.items.length,
-          itemBuilder: (_, i) => Column(
-            children: [
-              AdminCouponItem(
-                couponsData.items[i].id,
-                couponsData.items[i].title,
-                couponsData.items[i].imageUrl,
-              ),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(
+            (10),
+          ),
+          child: ListView.builder(
+            itemCount: couponsData.items.length,
+            itemBuilder: (_, i) => Column(
+              children: [
+                AdminCouponItem(
+                  couponsData.items[i].id,
+                  couponsData.items[i].title,
+                  couponsData.items[i].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
